9dc08c869264c1b129d091ef3cbfb66838024e47
Deelma <156914350+Deelma@users.noreply.github.com>
2024-11-19 08:43:36 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,23 +10,20 @@
     <div id="allshi">
     <header>
         <img id="logomale" src="yecinema.png" alt="LOGO">
-        <h1>YeCinema</h1>
+
         <a href="index.php">Strona główna</a><br>
         <a href="tabele.php">Baza danych</a><br>
     </header>
     <nav>
         <h2><b>Witamy w YeCinema!</b></h2><br>
         <form method="post" >
-            <br>
             <h2>Dodaj film</h2>
             Tytuł: <input type="text" name="tytul"><br>
             Reżyser: <input type="text" name="rezyser"><br>
             Czas trwania: <input type="number" min="0" name="czas_trwania"><br>
             <input type="submit" value="dodaj"><br><br>
         </form>
-        
     </nav>
-    
     <div id="animacja">
         <marquee behavior="reverse" id="animacja" scrollamount="11">
                     Lorem ipsum dolor sit amet cosik.
--- tabele.php
+++ tabele.php
@@ -10,13 +10,11 @@
     <div id="allshi">
     <header>
         <img id="logomale" src="yecinema.png" alt="LOGO">
-        <h1>YeCinema</h1>
         <a href="index.php">Strona główna</a><br>
         <a href="tabele.php">Baza danych</a><br>
     </header>
     <nav>
         <h2><b>Witamy w YeCinema!</b></h2><br>
-
         <form method="post">
             <h2>Wyswietl tabele</h2>
             <select name="tabele">
@@ -47,12 +45,10 @@
         $result = $conn->query($comm);
         if($result->num_rows > 0){
          while($row = $result->fetch_assoc()){
-             echo "id: " . $row["id"] . " |  seans_id: " . $row["seans_id"] . " | id sprzedawcy: " . $row["sprzedawca_id"] . " | ID klienta: " . $row["klient_id"] . " | cena: " . $row["cena"] . "<br>";
+             echo "id: " . $row["id"] . " |  seans_id: " . $row["seans_id"] . " | id sprzedawcy: " . $row["sprzedawca_id"] . " | klient_id: " . $row["klient_id"] . " | cena: " . $row["cena"] . "<br>";
          }
      }
-    }
-
-    if($_POST["tabela"] == "filmy"){
+    }elseif($_POST["tabela"] == "filmy"){
         $comm = "SELECT * FROM filmy";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -60,9 +56,7 @@
              echo "id: " . $row["id"] . " | tytuł: " . $row["tytul"] . " | rezyser: " . $row["rezyser"] . " | czas_trwania: " . $row["czas_trwania"] . "<br>";
          }
      }
-    }
-
-    if($_POST["tabela"] == "filmy_rodzaj"){
+    }elseif($_POST["tabela"] == "filmy_rodzaj"){
         $comm = "SELECT * FROM filmy_rodzaj";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -70,9 +64,7 @@
              echo "id: " . $row["id"] . " | filmy_id: " . $row["filmy_id"] . " | rodzaj_id: " . $row["rodzaj_id"];
          }
      }
-    }
-    
-    if($_POST["tabela"] == "klienci"){
+    }elseif($_POST["tabela"] == "klienci"){
         $comm = "SELECT * FROM klienci";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -80,9 +72,7 @@
              echo "id: " . $row["id"] . " | imie: " . $row["imie"] . " | nazwisko: " . $row["nazwisko"] . " | mail: " . $row["mail"] . "<br>";
          }
      }
-    }
- 
-    if($_POST["tabela"] == "rodzaj_filmu"){
+    }elseif($_POST["tabela"] == "rodzaj_filmu"){
         $comm = "SELECT * FROM rodzaj_filmu";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -90,9 +80,7 @@
              echo "id: " . $row["id"] . " | nazwa: " . $row["nazwa"] . "<br>";
          }
      }
-    }
-
-    if($_POST["tabela"] == "sale"){
+    }elseif($_POST["tabela"] == "sale"){
         $comm = "SELECT * FROM sale";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -100,9 +88,7 @@
              echo "id: " . $row["id"] . " | ilosc miejsc: " . $row["ilosc_miejsc"] . "<br>";
          }
      }
-    }
-
-    if($_POST["tabele"] == "seanse"){
+    }elseif($_POST["tabele"] == "seanse"){
         $comm = "SELECT * FROM seanse";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -110,9 +96,7 @@
              echo "id: " . $row["id"] . " | termin: " . $row["termin"] . " | sala_id: " . $row["sala_id"] . " | film_id: " . $row["film_id"] . " | liczba_wolnych_miejsc: " . $row["liczba_wolnych_miejsc"] . "<br>";
          }
      }
-    }
-
-    if($_POST["tabela"] == "sprzedawcy"){
+    }elseif($_POST["tabela"] == "sprzedawcy"){
         $comm = "SELECT * FROM sprzedawcy";
         $result = $conn->query($comm);
         if($result->num_rows > 0){
@@ -125,7 +109,7 @@
  
 ?>
     </nav>
-     <div id="animacja">
+     <div id="animacjaa">
         <marquee behavior="reverse" id="animacja" scrollamount="11">
                     Lorem ipsum dolor sit amet cosik.
         </marquee>
