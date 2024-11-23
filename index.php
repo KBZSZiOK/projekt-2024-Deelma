428f0bd7f1daf914a3752f9b9fb048da18bb87d9
Deelma <156914350+Deelma@users.noreply.github.com>
2024-11-23 19:15:07 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,12 +10,15 @@
     <div id="allshi">
     <header>
         <img id="logomale" src="yecinema.png" alt="LOGO">
-
-        <a href="index.php">Strona główna</a><br>
-        <a href="tabele.php">Baza danych</a><br>
     </header>
     <nav>
-        <h2><b>Witamy w YeCinema!</b></h2><br>
+        <ul>    
+            <li><a href="index.php">Dodawanie tabeli</a></li><br>
+            <li><a href="tabele.php">Baza danych</a></li><br>
+        </ul>
+    </nav>
+    <main>
+        <section><h2><b>Witamy w YeCinema!</b></h2><br>
         <form method="post">
             <h2>Dodaj film</h2>
             Tytuł: <input type="text" name="tytul"><br>
@@ -23,20 +26,42 @@
             Czas trwania: <input type="number" min="0" name="czas_trwania"><br>
             <input type="submit" value="dodaj"><br><br>
         </form>
-    </nav>
+        </section>
+        </main>
     <div id="animacja">
-        <marquee behavior="reverse" id="animacja" scrollamount="11">
-                    Lorem ipsum dolor sit amet cosik.
-        </marquee>
+        <div id="animacja-content">
+            Lorem ipsum asdasdas
+        </div>
     </div>
     <footer>
         <h4>2024 Playboy Carti<br>
         <b>Strone wykonał: Nikodem Warmowski</b></h4>
     </footer>
 </body>
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    const marquee = document.getElementById('animacja');
-});
-</script>
+<script src="script.js"></script>
 </html>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kino_4tig1";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+if(isset($_POST['tytul']) && isset($_POST['rezyser']) && isset($_POST['czas_trwania'])){
+$tytul = $_POST['tytul'];
+$rezyser = $_POST['rezyser'];
+$czas_trwania = $_POST['czas_trwania'];
+$sql = "INSERT INTO filmy (tytul, rezyser, czas_trwania) VALUES ('$tytul', '$rezyser', '$czas_trwania')";
+if(mysqli_query($conn, $sql)){
+    echo "Rekordy dodane.";
+}else{
+    echo "ERROR" . mysqli_error($conn);
+}
+}
+
+?> 
---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const animacjacontent = document.getElementById('animacja-content');
+    const animacja = document.getElementById('animacja');
+    let szerokosc = animacja.offsetWidth;
+    let szerokosccontent = animacjacontent.offsetWidth;
+    let startPos = szerokosc;
+    const predkosc = 2;  // Prędkość przesuwania w px
+
+    function animation() {
+        start -= predkosc;
+        if (start <= -szerokosccontent) {
+            start = szerokosc;
+        }
+        animacjacontent.style.left = `${start}px`;
+    }
+
+    function wlaczanimacje() {
+        szerokosc = animacja.offsetWidth;
+        szerokosccontent = animacjacontent.offsetWidth;
+        start = szerokosccontent;
+        animacjacontent.style.position = 'absolute';
+        animacjacontent.style.left = `${start}px`;
+    }
+
+    window.onload = function() {
+        wlaczanimacje();
+        setInterval(animation, 1);
+    };
+    window.onresize = wlaczanimacje;
+});
